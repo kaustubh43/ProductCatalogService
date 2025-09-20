8edd9158d1ac7545f162fcecde75d573eb360801
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2025-09-20 20:07:18 +0530

    initialize Product Catalog Service with basic models, controllers, and configuration

---
+++ src/main/java/org/example/productcatalogservice/controller/CategoryController.java
@@ -0,0 +1,59 @@
+package org.example.productcatalogservice.controller;
+
+
+import org.example.productcatalogservice.models.Category;
+import org.example.productcatalogservice.models.State;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Controller for handling category-related endpoints.
+ */
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+    /**
+     * Get Category by their ID passed as a path variable.
+     * @return Category object
+     */
+    @GetMapping("/{id}")
+    Category getCategoryById(@PathVariable int id) {
+        Category category = new Category();
+        category.setId(id);
+        return category;
+    }
+
+    /**
+     * Post a new category
+     * @return category
+     */
+    @PostMapping("/")
+     Category createCategory(@RequestBody Category category){
+        return category;
+    }
+
+    /**
+     * Update an existing category
+     */
+    @PutMapping("/update")
+    Category updateCategory(@RequestBody Category category){
+        return category;
+    }
+
+    /**
+     * Patch an existing category
+     */
+    @PatchMapping("/patch")
+    Category patchCategory(@RequestBody Category category){
+        return category;
+    }
+
+    /**
+     * Delete an existing category
+     */
+    @DeleteMapping("/delete")
+    Category deleteCategory(@RequestBody Category category){
+        category.setState(State.DELETED);
+        return category;
+    }
+}
---
+++ src/main/java/org/example/productcatalogservice/controller/ProductController.java
@@ -0,0 +1,61 @@
+package org.example.productcatalogservice.controller;
+
+import org.example.productcatalogservice.models.Product;
+import org.example.productcatalogservice.models.State;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Controller for handling product-related endpoints.
+ */
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    /**
+     * Get Product by their ID passed as a path variable.
+     * @return Product
+     */
+    @GetMapping("/{id}")
+    Product getProductById(@PathVariable("id") Long productId){
+        Product product = new Product();
+        product.setId(productId);
+        return product;
+    }
+
+    /**
+     * Add a product.
+     * @return product
+     */
+    @PostMapping("/")
+    Product addProduct(@RequestBody Product product){
+        return product;
+    }
+
+    /**
+     * Put a product
+     * @return product
+     */
+    @PutMapping("/update")
+    Product updateProduct(@RequestBody Product product){
+        return product;
+    }
+
+    /**
+     * Patch a product
+     * @return Product
+     */
+    @PatchMapping("/patch")
+    Product patchProduct(@RequestBody Product product){
+        return product;
+    }
+
+    /**
+     * Delete a product
+     * @return product
+     */
+    @DeleteMapping("/delete")
+    Product deleteProduct(@RequestBody Product product){
+        product.setState(State.DELETED);
+        return product;
+    }
+}
