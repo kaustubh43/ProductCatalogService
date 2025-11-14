3710a1e55eb2d3137a542b0a86a19b4a1d9483e2
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2025-11-14 21:51:30 +0530

    Enable JPA auditing; add createdAt and lastUpdatedAt fields to BaseModel

--- src/main/java/org/ecommerce/productcatalogservice/models/BaseModel.java
+++ src/main/java/org/ecommerce/productcatalogservice/models/BaseModel.java
@@ -1,13 +1,14 @@
 package org.ecommerce.productcatalogservice.models;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
@@ -16,11 +17,19 @@ import java.util.Date;
 @NoArgsConstructor
 @SuperBuilder
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
     private Date createdAt;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
     private Date lastUpdatedAt;
+
     private State state;
 }
